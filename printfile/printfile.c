c25d2c0ce24e0666829dcc793a3f48adc76897c0
Patrick Punch <punchp@oregonstate.edu>
2025-01-29 11:17:56 -0800

    in class challenge 4.3

---
+++ printfile/printfile.c
@@ -0,0 +1,29 @@
+/*
+Patrick Punch
+1/29/2025
+CS 374
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void)
+{
+    int fd = open("output.txt", O_CREAT | O_TRUNC | O_WRONLY, 0644);
+    if (fd == -1){
+        perror("open");
+        exit(1);
+    }
+
+    int pfd = dup2(fd, 1);
+    if (pfd == -1){
+        perror("dup2");
+        close(fd);
+        exit(1);
+    }
+
+    printf("Hello, world\nThis is a test!\n");
+    fflush(stdout);
+    close(fd);
+}
